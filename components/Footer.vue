5b0da7b5436e205a6a03e3a8e2a299d4c195efcc
Kirill Tepljakov <super.css2017@yandex.ru>
2024-12-29 16:38:22 +0300

    add files

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">
+    <div class="container">Footer</div>
+  </div>
+</template>
+<style>
+.footer {
+  width: 100%;
+  background-color: black;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">
+    <div class="container">Header</div>
+  </div>
+</template>
+<style>
+.header {
+  width: 100%;
+  background-color: white;
+}
+</style>
